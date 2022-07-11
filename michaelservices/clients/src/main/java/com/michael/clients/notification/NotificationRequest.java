2576df67115b8adfe6f7f6c2a13872a01a8c85b3
Michael-Royf <81159914+Michael-Royf@users.noreply.github.com>
2022-07-11 17:54:48 +0300

    add  Zipkin

--- michaelservices/clients/src/main/java/com/michael/clients/notification/NotificationRequest.java
+++ michaelservices/clients/src/main/java/com/michael/clients/notification/NotificationRequest.java
@@ -2,12 +2,14 @@ package com.michael.clients.notification;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @Builder
-public class NotificationRequest {
+public class NotificationRequest implements Serializable {
     private Integer toCustomerId;
     private String toCustomerEmail;
     private String message;
